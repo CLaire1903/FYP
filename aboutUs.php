dbcfba2e774107e44c1b117ded9a165f1cf4f52b
Tang Jia Huey <tangjiahuey6738@e.newera.edu.my>
2022-08-02 02:38:24 +0800

    css changed

--- aboutUs.php
+++ aboutUs.php
@@ -5,7 +5,6 @@
     <title>About Us</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href="css/shared.css" rel="stylesheet">
-    <link href="css/product.css" rel="stylesheet">
 
     <style>
         #aboutUs {
